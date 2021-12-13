f372e89d94e5d1bb01edcd4facbaccc3f576bee0
Muneeb Muhammad <77430152+MuneebMuhammad@users.noreply.github.com>
2021-12-13 10:04:30 +0500

    Update ForwardIndex.py
    removed author and source variables

--- ForwardIndex.py
+++ ForwardIndex.py
@@ -1,9 +1,6 @@
 class ForwardIndexing(object):
 
-    # constructor to initialize source and author of publication of an article
-    def __init__(self, srce, athr):
-        self.source = srce
-        self.author = athr
+    def __init__(self):
         self.title = {}  # stores location of each word in title
         self.content = {}   # stores location of each word in content
 

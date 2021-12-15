152fcfff0e15012a0e888025d5f634c0beb5d3af
Muneeb Muhammad <77430152+MuneebMuhammad@users.noreply.github.com>
2021-12-16 01:19:16 +0500

    Update main.py
    Forward Index created and stored in barrels. Takes 22 minutes to store forward index.

--- main.py
+++ main.py
@@ -1,107 +1,112 @@
 import json  # how to load json files
 import pickle
-import re
 import os
-import time
-import nltk
-from nltk import word_tokenize
-from nltk.corpus import stopwords
 from pathlib import Path
+import ForwardIndex
 from nltk.stem.snowball import SnowballStemmer
 import time
-import string
+
+# divide one article into barrels
+def create_forwardindex(fwdix, single):
+    for wid, hits in fwdix.items():
+        binid = wid // 500  # div of wid by 500 will be our binid in which the word id will be stored
+        with open(str(binid)+'.txt', 'a') as addobj:
+            addobj.write(single['url'] + " " + str(wid) + " " + str(hits) + "\n")
 
 
-def updateLexicon(obj):
-    # parse through every article in each document
+
+def update_data(obj):
     for i in range(len(obj)):
-        # parse through each character in title and collect alphabetical words with size greater than 3
+        fx = {}
+        loc = 0
+        # add source of the article to the lexicon and forwardIndex dictionary
+        srce = obj[i]['source']
+        if srce not in lexicon:
+            lexicon[srce] = [lexicon[list(lexicon)[-1]][0] + 1, 1]
+        else:
+            lexicon[srce][1] += 1
+        hit = [1, loc]
+        wid = lexicon[list(lexicon)[-1]][0]
+        fx[lexicon[list(lexicon)[-1]][0]] = [hit]
+
+        # get title from one article
         t = obj[i]['title']
         word = ""
         loc = 0
-        for char in t:
-            # append characters is character is alphabet
-            if char.isalpha():
-                word += char
-            else:
-                # if word size is less than 3 then discharge the word and start with new word
-                if len(word) <= 3:
-                    word = ""    # location can be incremented here *********
-                    loc = loc + 1
-                    continue
-                word = word.lower()  # convert word to lower case
-                word = snow_stemmer.stem(word)  # perform stemming on the word
-                # if word is not in lexicon then add the word to lexicon with 'word id = word_id of last element in dictionary + 1'
-                if word not in lexicon:
-                    lexicon[word] = lexicon[(list(lexicon)[-1])] + 1
-                wid = lexicon[word]
-                hit = [1, loc]   # find hit. '1' shows fancy hit. '0' shows plain hit
-                # add the word along with its location and article's url to inverted index
-                if wid not in ix:  # changed ****
-                    ix[wid] = {obj[i]['url']: [hit]}
-                else:
-                    if obj[i]['url'] in ix[wid]:
-                        ix[wid][obj[i]['url']].append(hit)
-                    else:
-                        ix[wid][obj[i]['url']] = [hit]  # till here
-                loc += 1
-                word = ""
-
-        # perform the same operation with content data of the article
-        c = obj[i]['content']
-        word = ""
-        loc = 0
-        for char in c:
-            if char.isalpha():
-                word += char
+        # parse through title and select relevant words
+        for cr in t:
+            if cr.isalpha():
+                word += cr
             else:
                 if len(word) <= 3:
                     word = ""
-                    loc += 1
                     continue
-                word = word.lower()
+                word = word.lower()  # convert word to lower case
                 word = snow_stemmer.stem(word)
+                # if word is not in lexicon then this word is added to lexicon with new word-id and '1' word count
                 if word not in lexicon:
-                    lexicon[word] = lexicon[(list(lexicon)[-1])] + 1
-                wid = lexicon[word]
-                hit = [0, loc]
-                # add the word along with its hits and article's url to inverted index
-                if wid not in ix:
-                    ix[wid] = {obj[i]['url']: [hit]}
+                    lexicon[word] = [lexicon[list(lexicon)[-1]][0] +1, 1]
+                # if word already exists then increment word count of that word
                 else:
-                    if obj[i]['url'] in ix[wid]:
-                        ix[wid][obj[i]['url']].append(hit)
-                    else:
-                        ix[wid][obj[i]['url']] = [hit]
-                loc += 1
+                    lexicon[word][1] += 1
+
+                wid = lexicon[word][0]
                 word = ""
+                hit = [1, loc] #find hit. '1' shows fancy hit, '0' shows plain hit. also add location of word in article
+                loc += 1
+                if wid not in fx:
+                    fx[wid] = [hit]
+                else:
+                    fx[wid].append(hit)
 
+        # get content from one article
+        c = obj[i]['content']
+        word = ""
+        loc = 0
+        # parse through content and select relevant words
+        for cr in c:
+            if cr.isalpha():
+                word += cr
+            else:
+                if len(word) <= 3:
+                    word = ""
+                    continue
+                word = word.lower()  # convert word to lower case
+                word = snow_stemmer.stem(word)
+                # if word is not in lexicon then this word is added to lexicon with new word-id and '1' word count
+                if word not in lexicon:
+                    lexicon[word] = [lexicon[list(lexicon)[-1]][0] + 1, 1]
+                # if word already exists then increment word count of that word
+                else:
+                    lexicon[word][1] += 1
+                wid = lexicon[word][0]
+                word = ""
+                hit = [0, loc]  # find hit. '1' shows fancy hit, '0' shows plain hit. also add location of word in article
+                loc += 1
+                if wid not in fx:
+                    fx[wid] = [hit]
+                else:
+                    fx[wid].append(hit)
+
+        create_forwardindex(fx, obj[i])  # create forward index from fx dictionary
+
+
+lexicon = {'told': [0, 0]}  # set first word in lexicon. first element in array is word id second element is frequency
 snow_stemmer = SnowballStemmer(language='english')
-start_time = time.time()
-# update lexicon, forward index and inverted index for every document
 
-lexicon = {'told': 0}
-ix = {}
 cwd = os.getcwd()
 cwd += '/newsdata'
+start_time = time.time()
 files = [o for o in os.listdir(cwd) if o.endswith('.json')]   # get all json files in newsdata directory
-# parse through each file and find inverted index
 for f in files:
     myjsonfile = open(cwd+'/'+f, 'r')
     jsondata = myjsonfile.read()
-    obj = json.loads(jsondata)
+    fileobj = json.loads(jsondata)
     myjsonfile.close()
-    updateLexicon(obj)
+    update_data(fileobj)
     print(f)
 
-# save lexicon in a file
 a_file = open("Lexicon.pkl", "wb")
 pickle.dump(lexicon, a_file)
 a_file.close()
-
-# save lexicon in a file
-a_file = open("invertedindex.pkl", "wb")
-pickle.dump(ix, a_file)
-a_file.close()
-
-print("time:", time.time() - start_time, " seconds")
+print("time:", time.time()-start_time, "seconds")

b19fa37c3af1e2d3d2d54bdb3cb1c4770bd0302d
Muneeb Muhammad <77430152+MuneebMuhammad@users.noreply.github.com>
2021-12-19 10:52:29 +0500

    Update main.py
    Assigned weights to hits

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def update_data(obj):
 
                 wid = lexicon[word][0]
 
-                hit = [1, loc] # make hit: '1' shows fancy hit, also add location of word in article
+                hit = [5, loc] # make hit: '1' shows fancy hit, also add location of word in article
                 loc += 1
                 # if the word is found first time in an article then add its hit to fx and increment that word frequency
                 if wid not in fx:
@@ -126,7 +126,7 @@ def update_data(obj):
                 wid = lexicon[word][0]
 
                 # find hit. '0' shows plain hit, also add location of word in article
-                hit = [0, loc]
+                hit = [2, loc]
                 loc += 1
                 if wid not in fx:
                     lexicon[word][1] += 1
